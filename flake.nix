f862fdfa665e136234a2932ecef386dc64ccad03
Maurice Gerczuk <maurice.gerczuk@uni-a.de>
2025-07-17 10:40:58 +0200

    Enhance python-env script to detect multiple package managers and improve error handling

--- flake.nix
+++ flake.nix
@@ -37,27 +37,66 @@
             copier
           ];
         };
-        packages.python-env = pkgs.writeShellApplication {
-          name = "python-env";
-          runtimeInputs = [pkgs.copier pkgs.git];
-          text = ''
-            # set -euo pipefail
+        packages = rec {
+          python-env = pkgs.writeShellApplication {
+            name = "python-env";
+            runtimeInputs = [pkgs.copier pkgs.git];
+            text = ''
+              # set -euo pipefail
 
-            if [ $# -ne 1 ]; then
-              echo "Usage: python-env <destination-directory>"
-              exit 1
-            fi
+              if [ $# -ne 1 ]; then
+                echo "Usage: python-env <destination-directory>"
+                exit 1
+              fi
 
-            TEMPLATE_DIR=${self}
-            DEST_DIR=$(realpath "$1")
+              TEMPLATE_DIR=${self}
+              dst_path=$(realpath "$1")
 
-            # Create a writable temp copy
-            TMP_TEMPLATE=$(mktemp -d)
-            cp -r "$TEMPLATE_DIR/." "$TMP_TEMPLATE"
-            chmod -R u+w "$TMP_TEMPLATE"
+              # List files in the destination path, or set to empty if directory does not exist
+              if [ -d "$dst_path" ]; then
+                files=$(ls "$dst_path")
+              else
+                files=""
+              fi
 
-            copier copy "$TMP_TEMPLATE" "$DEST_DIR"
-          '';
+              echo "Checking for python package manager files in $dst_path:"
+              if [[ $files == *"pyproject.toml"* ]]; then
+                  echo "Found pyproject.toml, using uv as package manager. Could also be poetry or pixi, but we default to uv."
+                  pythonPackageManager="uv"
+                  pythonProjectFileExists=true
+              fi
+              if [[ $files == *"uv.lock"* ]]; then
+                  echo "Found uv.lock, using uv as package manager."
+                  pythonPackageManager="uv"
+                  pythonProjectFileExists=true
+              fi
+
+              if [[ $files == *"pixi.lock"* ]] || [[ $files == *"pixi.toml"* ]]; then
+                  echo "Found pixi.lock or pixi.toml, using pixi as package manager."
+                  pythonPackageManager="pixi"
+                  pythonProjectFileExists=true
+              fi
+
+              if [[ $files == *"environment.yml"* ]]; then
+                  echo "Found environment.yml, using micromamba as package manager."
+                  pythonPackageManager="micromamba"
+                  pythonProjectFileExists=true
+              fi
+
+
+              pythonPackageManager=''${pythonPackageManager:- "uv"}
+              pythonProjectFileExists=''${pythonProjectFileExists:- false}
+
+
+              # Create a writable temp copy
+              TMP_TEMPLATE=$(mktemp -d)
+              cp -r "$TEMPLATE_DIR/." "$TMP_TEMPLATE"
+              chmod -R u+w "$TMP_TEMPLATE"
+
+              copier copy "$TMP_TEMPLATE" "$dst_path" --trust --data "_python_package_manager_default=$pythonPackageManager" --data "_python_project_file_exists=$pythonProjectFileExists"
+            '';
+          };
+          default = python-env;
         };
       };
     };
