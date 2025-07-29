2f9027008948adb9bd256fc339e8d43b95a585f7
Maurice Gerczuk <maurice.gerczuk@uni-a.de>
2025-07-29 13:47:35 +0200

    Inlude nixpkgs-python cache in flake nixConfig.

--- templates/devenv/{% if interface == "flake" %}.nix{% endif %}/flake.nix
+++ templates/devenv/{% if interface == "flake" %}.nix{% endif %}/flake.nix
@@ -19,8 +19,14 @@
   };
 
   nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
+    extra-trusted-public-keys = [
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
+    ];
+    extra-substituters = [
+      "https://devenv.cachix.org"
+      "https://nixpkgs-python.cachix.org"
+    ];
   };
 
   outputs = inputs @ {
